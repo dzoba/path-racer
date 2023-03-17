e1809e90053f3977e10c09604fac2fcf7daeb409
Chris Dzoba <Chris@Dzoba.com>
2023-03-17 09:15:56 -0400

    Mostly complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,167 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Pathfinding from "pathfinding";
+import "./App.css";
 
-function App() {
+const CELL_SIZE = 20;
+const MOVE_SPEED = 90;
+
+const App = () => {
+  const [grid, setGrid] = useState([]);
+  const [bluePos, setBluePos] = useState({ x: 0, y: 0 });
+  const [greenPos, setGreenPos] = useState({ x: 0, y: 0 });
+  const [targetPos, setTargetPos] = useState({ x: 0, y: 0 });
+  const [blueWins, setBlueWins] = useState(0);
+  const [greenWins, setGreenWins] = useState(0);
+  const [infoVisible, setInfoVisible] = useState(false);
+
+  const randomPosition = (maxX, maxY) => ({
+    x: Math.floor(Math.random() * maxX),
+    y: Math.floor(Math.random() * maxY),
+  });
+
+  const createGrid = (width, height) => {
+    const newGrid = new Array(height).fill(null).map(() =>
+      new Array(width).fill(0)
+    );
+
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        if (Math.random() < 0.2) {
+          newGrid[y][x] = 1;
+        }
+      }
+    }
+
+    return newGrid;
+  };
+
+  const initGame = () => {
+    const gridWidth = Math.floor(window.innerWidth / CELL_SIZE);
+    const gridHeight = Math.floor(window.innerHeight / CELL_SIZE);
+
+    const newGrid = createGrid(gridWidth, gridHeight);
+    setGrid(newGrid);
+
+    setBluePos(randomPosition(gridWidth, gridHeight));
+    setGreenPos(randomPosition(gridWidth, gridHeight));
+    setTargetPos(randomPosition(gridWidth, gridHeight));
+  };
+
+  const findPath = (startPos, endPos, grid) => {
+    const finder = new Pathfinding.AStarFinder();
+    const path = finder.findPath(
+      startPos.x,
+      startPos.y,
+      endPos.x,
+      endPos.y,
+      new Pathfinding.Grid(grid)
+    );
+    return path;
+  };
+
+  const move = (currentPos, nextPos) => {
+    if (nextPos) {
+      return { x: nextPos[0], y: nextPos[1] };
+    }
+    return currentPos;
+  };
+
+  useEffect(() => {
+    initGame();
+  }, []);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const bluePath = findPath(bluePos, targetPos, grid);
+      const greenPath = findPath(greenPos, targetPos, grid);
+
+      if (bluePath.length === 0 && greenPath.length === 0) {
+        clearInterval(intervalId);
+        initGame();
+      } else {
+        setBluePos(move(bluePos, bluePath[1]));
+        setGreenPos(move(greenPos, greenPath[1]));
+
+        if (bluePos.x === targetPos.x && bluePos.y === targetPos.y) {
+          clearInterval(intervalId);
+          setBlueWins(blueWins + 1);
+          initGame();
+        } else if (greenPos.x === targetPos.x && greenPos.y === targetPos.y) {
+          clearInterval(intervalId);
+          setGreenWins(greenWins + 1);
+          initGame();
+        }
+      }
+    }, MOVE_SPEED);
+
+    return () => clearInterval(intervalId);
+  }, [bluePos, greenPos, targetPos, grid]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div
+      className="grid"
+      style={{
+        width: grid[0]?.length * CELL_SIZE,
+        height: grid.length * CELL_SIZE,
+        gridTemplateColumns: `repeat(${grid[0]?.length || 0}, ${CELL_SIZE}px)`,
+        gridTemplateRows: `repeat(${grid.length}, ${CELL_SIZE}px)`,
+        transform: `scale(${Math.min(
+          window.innerWidth / (grid[0]?.length * CELL_SIZE),
+          window.innerHeight / (grid.length * CELL_SIZE)
+        )})`,
+        transformOrigin: "0 0",
+      }}
+    >
+      {grid.map((row, y) =>
+        row.map((cell, x) => (
+          <div
+            key={`cell-${x}-${y}`}
+            className={`cell ${cell === 1 ? "wall" : ""}`}
+            style={{
+              gridColumnStart: x + 1,
+              gridRowStart: y + 1,
+            }}
+          >
+            {bluePos.x === x && bluePos.y === y && (
+              <div className="blue" style={{ width: CELL_SIZE, height: CELL_SIZE }} />
+            )}
+            {greenPos.x === x && greenPos.y === y && (
+              <div className="green" style={{ width: CELL_SIZE, height: CELL_SIZE }} />
+            )}
+            {targetPos.x === x && targetPos.y === y && (
+              <div className="target" style={{ width: CELL_SIZE, height: CELL_SIZE }} />
+            )}
+          </div>
+        ))
+      )}
+      <div
+        className={`info-container ${infoVisible ? "expanded" : ""}`}
+        onClick={() => setInfoVisible(!infoVisible)}
+      >
+        <div className="info-circle">
+          {!infoVisible && <span className="question-mark">?</span>}
+          {infoVisible && (
+            <div className="info-content">
+              <div>Blue: {blueWins} Green: {greenWins}</div>
+              <div>Author: Chris Dzoba & GPT-4</div>
+              <div>
+                <a href="https://twitter.com/ChrisDzoba" target="_blank" rel="noopener noreferrer">
+                  Twitter
+                </a>{" "}
+                <a href="https://github.com/dzoba/path-racer" target="_blank" rel="noopener noreferrer">
+                  Github
+                </a>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
-}
+  
+  
+};
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -4,6 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC6UBs0LNDxf_z5L0lDK7lWmMyE1VgPzjg",
+  authDomain: "path-racer.firebaseapp.com",
+  projectId: "path-racer",
+  storageBucket: "path-racer.appspot.com",
+  messagingSenderId: "310517200678",
+  appId: "1:310517200678:web:6c470b13cfb4bd1b3db3e7",
+  measurementId: "G-G36SBVT6N6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
